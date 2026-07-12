a3f4e1ad75a0c4dbae02f337b05f389367a17d07
niki <lingtengtang1@gmail.com>
2026-07-12 17:13:27 +0800

    修复了按钮及其中的文字的闪烁问题；增加了MouseClickEvent()函数，用于判断鼠标点击活动，并使其可以复用与各个界面按钮的点击；增加了enum Screen 用于判断当前处在什么界面；增加了AppState结构体，用于存储运行相关信息

--- GluttonousSnake/main.cpp
+++ GluttonousSnake/main.cpp
@@ -3,6 +3,52 @@
 #include <iostream>
 using namespace std;
 
+enum Screen
+{
+    INITIALMENU,
+	GAME,
+	GAMEOVER,
+    GAMEWIN,
+    PAUSEPAGE
+};
+
+struct AppState
+{
+    bool isRunning = 1;
+    bool isGameStarted = 0;
+
+    int Mouse_X;
+    int Mouse_Y;
+    bool Mouse_Clicked = false;
+
+    Screen currentScreen = INITIALMENU;
+}Game;
+
+void MouseClickEvent(ExMessage msg)
+{
+    Game.Mouse_X = msg.x;
+    Game.Mouse_Y = msg.y;
+    Game.Mouse_Clicked = true;
+
+    switch (Game.currentScreen)
+    {
+		//handel the mouse click event for the initial menu screen
+        case INITIALMENU:
+            if (Game.Mouse_X >= BEGINBUTTON_X1 && Game.Mouse_X <= BEGINBUTTON_X2 &&
+                Game.Mouse_Y >= BEGINBUTTON_Y1 && Game.Mouse_Y <= BEGINBUTTON_Y2)
+            {
+                Game.currentScreen = GAME;
+            }
+            else if (Game.Mouse_X >= EXITBUTTON_X1 && Game.Mouse_X <= EXITBUTTON_X2 &&
+                Game.Mouse_Y >= EXITBUTTON_Y1 && Game.Mouse_Y <= EXITBUTTON_Y2)
+            {
+                Game.isRunning = false;
+            }
+		    break;
+            
+    }
+}
+
 static void DrawTitle()
 {
     settextstyle(TITLE_TEXT_H, 0, _T("Impact"));
@@ -40,70 +86,67 @@ static void DrawExitButton(bool hovered)
             COLOR_BUTTON_TEXT, _T("Consolas"), EXITBUTTON_TEXT_H);
 }
 
-void Initial()
+void DrawInitialMenu()
+{
+    DrawTitle();
+    bool inside_beginbutton = Game.Mouse_X >= BEGINBUTTON_X1 && Game.Mouse_X <= BEGINBUTTON_X2 &&
+        Game.Mouse_Y >= BEGINBUTTON_Y1 && Game.Mouse_Y <= BEGINBUTTON_Y2;
+    DrawBeginningButton(inside_beginbutton);
+
+    bool inside_exitbutton = Game.Mouse_X >= EXITBUTTON_X1 && Game.Mouse_X <= EXITBUTTON_X2 &&
+        Game.Mouse_Y >= EXITBUTTON_Y1 && Game.Mouse_Y <= EXITBUTTON_Y2;
+    DrawExitButton(inside_exitbutton);
+}
+
+
+int main()
 {
     initgraph(WINDOW_W, WINDOW_H);
     setbkcolor(COLOR_BG);
     cleardevice();
+	ExMessage MouseMsg;
 
-    DrawBeginningButton(false);
-    DrawExitButton(false);
+    BeginBatchDraw();
 
-    ExMessage msg;
-    bool Correct_Click = false;
-
-    while (1)
+    while (Game.isRunning)
     {
-        DrawTitle();
+        cleardevice();
 
-        if (peekmessage(&msg, EX_MOUSE))
+        switch (Game.currentScreen)
         {
-            int Mouse_X = msg.x;
-            int Mouse_Y = msg.y;
-            bool inside_beginbutton = Mouse_X >= BEGINBUTTON_X1 && Mouse_X <= BEGINBUTTON_X2 &&
-                Mouse_Y >= BEGINBUTTON_Y1 && Mouse_Y <= BEGINBUTTON_Y2;
+        case INITIALMENU:
+            DrawInitialMenu();
+            break;
+        case GAME:
+            // DrawGame();
+            break;
+        case GAMEOVER:
+            // DrawGameOver();
+            break;
+        case GAMEWIN:
+            // DrawGameWin();
+            break;
+        case PAUSEPAGE:
+            // DrawPausePage();
+            break;
+        }
 
-            if (msg.message == WM_LBUTTONDOWN)
+        FlushBatchDraw();
+
+        if (peekmessage(&MouseMsg, EX_MOUSE))
+        {
+            Game.Mouse_X = MouseMsg.x;
+			Game.Mouse_Y = MouseMsg.y;
+            if (MouseMsg.message == WM_LBUTTONDOWN)
             {
-                if (inside_beginbutton)
-                {
-                    break;
-                }
-            }
-            else if (inside_beginbutton != Correct_Click)
-            {
-                Correct_Click = inside_beginbutton;
-            }
-
-            DrawBeginningButton(inside_beginbutton);
-
-
-
-            bool inside_exitbutton = Mouse_X >= EXITBUTTON_X1 && Mouse_X <= EXITBUTTON_X2 &&
-                Mouse_Y >= EXITBUTTON_Y1 && Mouse_Y <= EXITBUTTON_Y2;
-
-            if (msg.message == WM_LBUTTONDOWN)
-            {
-                if (inside_exitbutton)
-                {
-                    break;
-                }
-            }
-            else if (inside_exitbutton != Correct_Click)
-            {
-                Correct_Click = inside_exitbutton;
-            }
-
-            DrawExitButton(inside_exitbutton);
-            
+                MouseClickEvent(MouseMsg);
+			}
         }
     }
 
-    closegraph();
-}
+    EndBatchDraw();
+	closegraph();
 
-int main()
-{
-    Initial();
+
     return 0;
 }
